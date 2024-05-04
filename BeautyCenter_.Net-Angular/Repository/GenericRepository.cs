1e2f4e603c45dc0f18ff1193524c91bd457c3a9e
menna osama <menna osama@gmail.com>
2024-05-04 19:40:56 +0300

    GenericReposatory

--- BeautyCenter_.Net-Angular/Repository/GenericRepository.cs
+++ BeautyCenter_.Net-Angular/Repository/GenericRepository.cs
@@ -1,7 +1,49 @@
-﻿namespace BeautyCenter_.Net_Angular.Repository
+﻿using BeautyCenter_.Net_Angular.Models;
+
+namespace BeautyCenter_.Net_Angular.Repository
 {
     public class GenericRepository<type> where type : class
     {
+        BeautyCenterContext db;
+
+
+        public GenericRepository(BeautyCenterContext db)
+        {
+          this.db =db;
+        }
+
+
+        public List<type> selectall()
+        {
+            return db.Set<type>().ToList();
+        }
+
+        public type selectbyid(int id)
+        {
+            return db.Set<type>().Find(id);
+        }
+
+        public void add(type entity)
+        {
+            db.Set<type>().Add(entity);
+
+        }
+
+        public void update(type entity)
+        {
+            db.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        }
+
+        public void delete(int id)
+        {
+            type obj = db.Set<type>().Find(id);
+            db.Set<type>().Remove(obj);
+        }
+
+        public void save()
+        {
+            db.SaveChanges();
+        }
 
     }
 }
---
+++ BeautyCenter_.Net-Angular/UnitOfWork/unitOfWork.cs
@@ -0,0 +1,85 @@
+﻿using BeautyCenter_.Net_Angular.Models;
+using BeautyCenter_.Net_Angular.Repository;
+
+namespace BeautyCenter_.Net_Angular.UnitOfWork
+{
+    public class UnitOfWork
+    {
+        BeautyCenterContext db;
+        GenericRepository<Package> packageRepository;
+        GenericRepository<PackageUser> packageUserRepository;
+        GenericRepository<Service> serviceRepository;
+        GenericRepository<Userr> userRepository;
+        GenericRepository<UserService> userServiceRepository;
+
+        public UnitOfWork(BeautyCenterContext db)
+        {
+            this.db = db;
+        }
+
+        public GenericRepository<Package> PackageRepository
+        {
+            get
+            {
+                if (packageRepository == null)
+                {
+                    packageRepository = new GenericRepository<Package>(db);
+                }
+                return packageRepository;
+            }
+        }
+
+        public GenericRepository<PackageUser> PackageUserRepository
+        {
+            get
+            {
+                if (packageUserRepository == null)
+                {
+                    packageUserRepository = new GenericRepository<PackageUser>(db);
+                }
+                return packageUserRepository;
+            }
+        }
+
+        public GenericRepository<Service> ServiceRepository
+        {
+            get
+            {
+                if (serviceRepository == null)
+                {
+                    serviceRepository = new GenericRepository<Service>(db);
+                }
+                return serviceRepository;
+            }
+        }
+
+        public GenericRepository<Userr> UserRepository
+        {
+            get
+            {
+                if (userRepository == null)
+                {
+                    userRepository = new GenericRepository<Userr>(db);
+                }
+                return userRepository;
+            }
+        }
+
+        public GenericRepository<UserService> UserServiceRepository
+        {
+            get
+            {
+                if (userServiceRepository == null)
+                {
+                    userServiceRepository = new GenericRepository<UserService>(db);
+                }
+                return userServiceRepository;
+            }
+        }
+
+        public void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+    }
+}
